690828492ae613491c57840c7b526bb6f65e5fec
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2020-12-31 13:03:08 -0400

    Add birthdate property to customer model as nullable datetime

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -12,7 +12,13 @@ namespace Vidly.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+
         public bool IsSubscribedToNewsletter { get; set; }
+
+        [Display(Name = "Membership Type")]
         public MembershipType MembershipType { get; set; }
         public byte MembershipTypeId { get; set; }
     }
